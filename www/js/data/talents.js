b953a663aa5a8577880ae3d5073c790f327e48c3
Rodrigo Camargo <Rodrigo Camargo>
2015-11-12 22:50:07 -0200

    Talents

---
+++ www/js/data/talents.js
@@ -0,0 +1,202 @@
+var talents = [
+
+  {
+    "hero": "Uther",
+    "lvl1":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image": "images/heroes/talents/uther/Conjurer's_Pursuit_Icon.png"
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl4":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl7":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl10":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl13":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl20":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    }
+  },
+  {
+    "hero": "Uther",
+    "lvl1":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl4":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl7":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl10":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl13":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    },
+    "lvl20":{
+      "1":{
+        "name": "Conjurers' Pursuit",
+        "description": "Collecting Regeneration Globes permanently increases Mana Regeneration by 0.1 per second.",
+        "image":
+      },
+      "2"{
+
+      },
+      "3":{
+
+      },
+      "4":{
+
+      }
+    }
+  },
+
+];
